3b1b3a985e325c7338a7eaef686cc557b3ef51f5
julien-gournay <69372778+julien-gournay@users.noreply.github.com>
2022-05-25 23:36:17 +0200

    Add files via upload

--- js/affichage.js
+++ js/affichage.js
@@ -1,6 +1,8 @@
 // AFFICHAGE HEURE
 window.onload = function() {
-    horloge('div_heure');
+    horloge('div_heure'); //chargement automatique de l'heure
+    buttonClickGET(); //chargement automatique des valeurs
+    afficherDate()//chargement automatique de la date
 };
 
 function horloge(el) {
@@ -23,7 +25,7 @@ function pause(ms) {
 }
 async function afficherDate() {
     while (true) {
-        await pause(1000);
+        await pause(0); //temps de chargement
         var cejour = new Date();
         var options = { weekday: "long", year: "numeric", month: "long", day: "2-digit" };
         var date = cejour.toLocaleDateString("fr-FR", options);
@@ -33,13 +35,61 @@ async function afficherDate() {
         document.getElementById('div_date').innerHTML = dateheure;
     }
 }
-afficherDate();
+
+
+//function hinfo() {
+//    var journee = ;
+//    var soiree = ;
+//    if (new Date >= );
+//    else if ();
+//}
 
 
 
-function hinfo() {
-    var journee = ;
-    var soiree = ;
-    if (new Date >= );
-    else if ();
+var callBackGetSuccess = function(data) {
+    console.log("donnees api", data)
+    //alert("Meteo temp  : "  + data.data.iaqi.pm10.v);
+    var element3 = document.getElementById("v3"); //texte PM10
+    element3.innerHTML = data.data.iaqi.pm10.v  + " PPM" ; 
+    var element2 = document.getElementById("v2"); //texte NO2
+    element2.innerHTML = data.data.iaqi.no2.v + " PPM" ; 
 }
+
+function buttonClickGET() {
+    
+    var url = "https://api.waqi.info/feed/tourcoing/?token=513e8d565672413feab137d48078d6014d7816d0" //url api
+
+    $.get(url, callBackGetSuccess).done(function() {
+        //alert( "second success" );
+      })
+      .fail(function() {
+        alert( "Une ERREUR est survenue suite à la deconnexion de l'API ou à un problème dans le code." );
+      })
+      .always(function() {
+        //alert( "Les valeurs ont bien était mis a jour." );
+      });
+}
+
+setTimeout(() => {
+    window.location.reload(true);
+}, 60000); //rechargement de la page toute les minutes
+
+
+
+
+var today=new Date();
+var heure="";
+function messagevariable() {
+var time=today.getHours();
+heure=""
+if(time>=24 && time<6)
+heure="Bonne nuit !"
+if(time>=6 && time<13)
+heure="Bonne journée !"
+if(time>=13 && time<20)
+heure="Bon aprés midi !"
+if(time>=20 && time<24)
+heure="Bonne soirée"
+}
+//messagevariable();
+//document.writeln(heure) AFFICHER LE MESSAGE
